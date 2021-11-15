5a0cffe7171eed24a6f371d8a5009679411853b3
ankitt01 <ankitrawat0102@gmail.com>
2021-11-15 10:04:23 +0530

    Navigate component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 
 
 ReactDOM.render(
-<Router>
-  <Routes>
-    <Route path="/" element={Home}/>
-  </Routes>
-</Router>,
+  <Router>
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/myapps' element={<Learn />} />
+      <Route path='/learn' element={<Navigate replace to="/myapps" />} /> 
+      {/* without replace keyword the learn page comes on top  */}
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
 
@@ -18,6 +21,14 @@ function Home() {
   return (
     <div>
       <h1>Home Route</h1>
+    </div>
+  )
+}
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed here</h4>
     </div>
   )
 }
