9e3d37723027bf517df143ac31535fce33a5491a
ankitt01 <ankitrawat0102@gmail.com>
2021-11-15 10:20:12 +0530

    Link component and nested routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom'
 
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path='/' exact element={<Home />} />
-      <Route path='/myapps' element={<Learn />} />
-      <Route path='/learn' element={<Navigate replace to="/myapps" />} /> 
+      <Route path='/myapps' element={<Navigate replace to="/learn" />} />
       {/* without replace keyword the learn page comes on top  */}
+      <Route path='/learn' element={<Learn />}>
+        <Route path='bundles' element={<Bundles />} />
+        <Route path='courses' element={<Courses />} />
+      </Route>
     </Routes>
   </Router>,
   document.getElementById('root')
@@ -29,6 +32,28 @@ function Learn() {
     <div>
       <h1>Learn</h1>
       <h4>All courses are listed here</h4>
+      <Link to="/learn/courses">Courses</Link> |
+      <Link to="/learn/bundles"> Bundle</Link>
+      {/* Link is used over <a> tag because by using Link the page does not reloads */}
+    </div>
+  )
+}
+
+
+function Bundles() {
+  return (
+    <div>
+      <h1>Bundle List</h1>
+      <h4>Bundle Card</h4>
+    </div>
+  )
+}
+
+function Courses() {
+  return (
+    <div>
+      <h1>Course List</h1>
+      <h4>Course Card</h4>
     </div>
   )
 }
